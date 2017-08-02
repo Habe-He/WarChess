7672885b73738cfbb3220142d2cefb86541d2983
Habe-He <892988508@qq.com>
2017-08-02 18:21:12 +0800

    remove android build file

--- proj.android/obj/local/armeabi/objs-debug/cocostudio_static/CCArmatureDefine.o.d
+++
@@ -1,5 +0,0 @@
-./obj/local/armeabi/objs-debug/cocostudio_static/CCArmatureDefine.o: \
-  /Users/ouyangwang/Documents/cocos2d-x-3.12/projects/WarChess/proj.android/../cocos2d/cocos/editor-support/cocostudio/CCArmatureDefine.cpp \
-  /Users/ouyangwang/Documents/cocos2d-x-3.12/projects/WarChess/proj.android/../cocos2d/cocos/./editor-support/cocostudio/CCArmatureDefine.h
-
-/Users/ouyangwang/Documents/cocos2d-x-3.12/projects/WarChess/proj.android/../cocos2d/cocos/./editor-support/cocostudio/CCArmatureDefine.h:
--- proj.android/obj/local/armeabi/objs-debug/recast_static/fastlz/fastlz.o.d
+++
@@ -1,2 +0,0 @@
-./obj/local/armeabi/objs-debug/recast_static/fastlz/fastlz.o: \
-  /Users/ouyangwang/Documents/cocos2d-x-3.12/projects/WarChess/proj.android/../cocos2d/external/recast/fastlz/fastlz.c
